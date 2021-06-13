5c22adc19882f409dc242bcfe8b2b78b3f06c969
ThreadsofDaemonS <vbnzuf@gmail.com>
2021-06-13 20:39:57 +0300

    first commit

---
+++ src/Technologies.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Technologies = () => {
+  return (
+    <div>
+      <ul>
+        <li>css</li>
+        <li>HTML</li>
+        <li>JSX</li>
+        <li>React</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Technologies;

1c6b82327daef32edfb4ef0add50b47748f2280b
ThreadsofDaemonS <vbnzuf@gmail.com>
2021-06-19 19:35:04 +0300

    work with css modules

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,20 +1,20 @@
 import React from "react";
+import s from "./Profile.module.css";
 
 const Profile = () => {
   return (
-    <div className="content">
+    <div className={s.content}>
       <div>
-        <img
-          src="https://bipbap.ru/wp-content/uploads/2017/08/16.jpg"
-          className="img-content"
-        />
+        <img src="https://bipbap.ru/wp-content/uploads/2017/08/16.jpg" />
       </div>
       <div>ava + description</div>
       <div>
         My posts
-        <div>New posts</div>
-        <div>Post 1</div>
-        <div>Post 2</div>
+        <div>New post</div>
+        <div className={s.posts}>
+          <div className={s.item}>Post 1</div>
+          <div className={s.item}>Post 2</div>
+        </div>
       </div>
     </div>
   );

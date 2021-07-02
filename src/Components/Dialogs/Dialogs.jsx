8494a54fc34b6a3300f548c8c26e12f88166f783
ThreadsofDaemonS <vbnzuf@gmail.com>
2021-07-02 15:27:09 +0300

    add message from dialogs page

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -3,6 +3,10 @@ import { NavLink } from "react-router-dom";
 import DialogsItem from "./DialogItem/DialogsItem";
 import s from "./Dialogs.module.css";
 import Message from "./Message/Message";
+import {
+  updateNewMessageTextActionCreator,
+  addMessageActionCreater,
+} from "../../redux/state";
 
 const Dialogs = (props) => {
   let dialogsElements = props.state.dialogs.map((d) => (
@@ -13,30 +17,35 @@ const Dialogs = (props) => {
     <Message message={m.message} />
   ));
 
-  let newMessageElement = React.createRef();
+  // let newMessageElement = React.createRef();
 
   let addMessage = () => {
-    props.addMessage();
+    props.dispatch(addMessageActionCreater());
   };
 
-  let onMessageChange = () => {
-    let text = newMessageElement.current.value;
-    props.updateNewMessageText(text);
+  let onMessageChange = (e) => {
+    let text = e.target.value;
+    props.dispatch(updateNewMessageTextActionCreator(text));
   };
 
   return (
     <div className={s.dialogs}>
       <div className={s.dialogsItems}>{dialogsElements}</div>
-      <div className={s.messages}>{messagesElements}</div>
-      <div>
-        <textarea
-          onChange={onMessageChange}
-          ref={newMessageElement}
-          value={props.newMessageText}
-        ></textarea>
-      </div>
-      <div>
-        <button onClick={addMessage}>Add message</button>
+      <div className={s.messages}>
+        {" "}
+        <div>{messagesElements}</div>
+        <div>
+          <div>
+            <textarea
+              onChange={onMessageChange}
+              //ref={newMessageElement}
+              value={props.newMessageText}
+            ></textarea>
+          </div>
+          <div>
+            <button onClick={addMessage}>Add message</button>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/Components/Profile/MyPosts/MyPosts.jsx
+++ src/Components/Profile/MyPosts/MyPosts.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import {
+  addPostActionCreater,
+  updateNewPostTextActionCreator,
+} from "../../../redux/state";
 import s from "./MyPosts.module.css";
 import Post from "./Post/Post";
 
@@ -10,12 +14,12 @@ const MyPosts = (props) => {
   let newPostElement = React.createRef();
 
   let addPost = () => {
-    props.addPost();
+    props.dispatch(addPostActionCreater());
   };
 
   let onPostChange = () => {
     let text = newPostElement.current.value;
-    props.updateNewPostText(text);
+    props.dispatch(updateNewPostTextActionCreator(text));
   };
 
   return (
